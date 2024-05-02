829c286bf2b9a7db64ff2fa01595eafc9e17bd96
NhanNguyen8080 <boiboii162122@gmail.com>
2024-05-02 12:25:33 +0700

    Add BrandController

---
+++ Backend/2Sport_BE/Controllers/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2Sport_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
